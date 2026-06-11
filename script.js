0830161eebf643645a6ae956243ca8fae037d2a0
BUSSO TEC <165737950+Juandesarrolla100@users.noreply.github.com>
2026-06-11 11:59:13 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const cursor = document.querySelector(".cursor-glow");
+
+document.addEventListener("mousemove",(e)=>{
+
+    cursor.style.left = e.clientX + "px";
+    cursor.style.top = e.clientY + "px";
+
+});
+
+console.log("B-TRON ACTIVADO");
